4f69494b70f2c500496bb64913465b2d29a0a8ea
Nathan Rose <mail.nathanrose@gmail.com>
2021-09-18 15:47:28 -0400

    removed forced sync at startup

--- rosecore/project/apps.py
+++ rosecore/project/apps.py
@@ -3,7 +3,3 @@ from django.apps import AppConfig
 
 class ProjectConfig(AppConfig):
     name = 'project'
-
-    def ready(self):
-        from .services import ProjectService
-        ProjectService.sync()
